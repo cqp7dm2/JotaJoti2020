175605d72a750bff2c63a3d14ba11c70472d356a
cqp7dm2 <44236908+cqp7dm2@users.noreply.github.com>
2020-08-27 14:42:22 +0800

    latest renew

--- qpof/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="bg-white sticky-footer">
-    <div class="container my-auto">
-        <div class="text-center my-auto copyright"><span>Copyright © Angel Food 2019 BY CHUA QIN PENG</span></div>
-    </div>
-</footer>
