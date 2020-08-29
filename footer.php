1a1c82e75ae13557cbf37e38b9ceea801ba8b353
darkchoceva <68815778+darkchoceva@users.noreply.github.com>
2020-08-29 18:36:01 +0800

    header & footer (eva)

---
+++ footer.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link href="style.css" rel="stylesheet" />
+    <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Palanquin+Dark:wght@500&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <!--<footer class="footer py-4">
+      <div class="container" style="height: 50px;">
+        <div class="row align-items-center mb-5">
+          <div class="col-lg-3 text-lg-left">Logo</div>
+          <div class="col-lg-3 text-lg-left">
+            <ul>
+
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Program</a></li>
+              <li><a href="#">Challenges</a></li>
+              <li><a href="#">Contact Us</a></li>
+            </ul>
+          </div>
+          <div class="col-lg-3 text-lg-left">
+
+          </div>
+          <div class="col-lg-3 my-3 my-lg-0">
+            <a class="btn btn-light btn-social mx-2" href="#!"><i class="fab fa-instagram"></i></a>
+            <a class="btn btn-light btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
+          </div>
+        </div>
+        <div class="row align-items-center">
+          <div class="col-lg-12 text-lg-center">Copyright © Jota Joti 2020</div>
+        </div>
+      </div>
+    </footer>-->
+
+    <div class="">
+    <section style="height:80px;"></section>
+    <footer class="footer-bs">
+        <div class="row">
+        	<div class="col-md-3 footer-brand animated fadeInLeft">
+            	<h2>Jota Joti 2020</h2>
+                <p>Suspendisse hendrerit tellus laoreet luctus pharetra. Aliquam porttitor vitae orci nec ultricies. Curabitur vehicula, libero eget faucibus faucibus, purus erat eleifend enim, porta pellentesque ex mi ut sem.</p>
+                <p>© 2014 BS3 UI Kit, All rights reserved</p>
+            </div>
+        	<div class="col-md-3 footer-nav animated fadeInUp">
+            	<h4>Menu —</h4>
+            	<div class="col-md-6">
+                    <ul class="list">
+                        <li><a href="#"><h4>Home</h4></a></li>
+                        <li><a href="#"><h4>About</h4></a></li>
+                        <li><a href="#"><h4>Program</h4></a></li>
+                        <li><a href="#"><h4>Challenges</h4></a></li>
+                        <li><a href="#"><h4>Contact Us</h4></a></li>
+                    </ul>
+                </div>
+            </div>
+        	<div class="col-md-2 footer-social animated fadeInDown">
+            	<h4>Follow Us</h4>
+            	<ul>
+                	<li><a href="#"><h4>Facebook</h4></a></li>
+                	<li><a href="#"><h4>Instagram</h4></a></li>
+                </ul>
+            </div>
+        	<div class="col-md-3 footer-ns animated fadeInRight">
+            <li><a href="#"><h4 style="color: #fff;">Terms and Condition</h4></a></li>
+            <li><a href="#"><h4 style="color: #fff;">FAQ</h4></a></li>
+            <li><a href="#"><h4 style="color: #fff;">Cookies Policy</h4></a></li>
+            <li><a href="#"><h4 style="color: #fff;">Privacy Policy</h4></a></li>
+            <li><a href="#"><h4 style="color: #fff;">Terms of Use</h4></a></li>
+          </div>
+        </div>
+    </footer>
+
+</div>
+
+  </body>
+</html>
--- header.php
+++ header.php
@@ -3,10 +3,50 @@
   <head>
     <meta charset="utf-8">
     <title></title>
+    <link href="style.css" rel="stylesheet" />
+    <!--<header class="container">
+      <li><a href="#" class="trans">Home</a></li>
+      <li><a href="#" class="trans">About</a></li>
+      <li><a href="#" class="trans">Program</a></li>
+      <li><a href="#" class="trans">Challenges</a></li>
+      <li><a href="#" class="trans">Contact Us</a></li>
+    </header>-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Palanquin+Dark:wght@500&display=swap" rel="stylesheet">
   </head>
+
   <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
 
-<p>Testing Mou</p>
+        <a class="navbar-brand" href="#">Jota Joti 2020</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link trans" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link trans" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link trans" href="#">Program</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link trans" href="#">Challenges</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link trans" href="#">Contact Us</a>
+            </li>
+          </ul>
+        </div>
 
+</nav>
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="scripts.js"></script>
   </body>
 </html>
