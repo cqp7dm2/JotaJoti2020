3014c6291249cb26de49fa886bf55ac2c68cb068
crosswind19 <70271667+crosswind19@users.noreply.github.com>
2020-08-30 19:24:54 +0800

    static page

--- about.php
+++ about.php
@@ -1,1 +1,40 @@
-a
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php include "header.php"; ?>
+
+    <header class="masthead">
+      <div class="container">
+              <div class="masthead-subheading">JOTA JOTI 2020</div>
+              <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>
+              <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
+          </div>
+    </header>
+
+    <section class="page-section cta">
+      <div class="container">
+        <div class="row">
+          <div class="col-xl-9 mx-auto">
+            <div class="cta-inner text-center rounded">
+              <h2 class="section-heading mb-4">
+                <span class="section-heading-lower">JOTA-JOTI</span>
+                <span class="section-heading-upper">Jamboree-on-the-Air-Jamboree-on-the-Internet</span>
+              </h2>
+              <p class="mb-0">JOTA-JOTI (Jamboree-on-the-Air-Jamboree-on-the-Internet) is the world’s largest digital Scout event taking place on the Internet and over the airwaves. Held ever year in October, the event connects millions of young people around the world for a full weekend of online activities that promote friendship and global citizenship. JOTA-JOTI enables young people and volunteers to participate in fun and engaging group activities over the Internet and amateur radio focused on developing 21st century skills through Scouting.
+
+This year JOTA-JOTI 2020 will take place from 16 to 18 October. The dynamic programme features a variety of non-formal education activities, including webinars, global campfires, talent shows, live shows, and much more. World Scouting’s global partners are actively involved in the event, offering their expertise and fun activities to engage participants. The JOTA-JOTI programme aims to support Scouts and non-Scouts of all ages to learn about communications technology, the values of global citizenship, and how together we can solve the most pressing challenges facing our planet.
+
+Keeping young people Safe from Harm online is an important aspect of this event. Through chat moderation, “listening ears”, e-learning courses and a number of sessions about how to be safe online, we make sure that all participants have a positive and inclusive JOTA-JOTI experience.
+
+JOTA-JOTI is one of World Scouting’s longest-running global events dating back more than 60 years to 1958. The digital educational event has seen steady annual growth in participation over the years and with a focus on the future aims to reach millions of more participants in the years to come.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </body>
+</html>
--- header.php
+++ header.php
@@ -41,7 +41,6 @@
         <div class="container">
             <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="assets/img/navbar-logo.svg" alt="" /></a>
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                Menu
                 <i class="fas fa-bars ml-1"></i>
             </button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
