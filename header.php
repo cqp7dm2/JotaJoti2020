7dc1e267ec3b1bf1356a71b019bc1a6e33370d75
cqp7dm2 <44236908+cqp7dm2@users.noreply.github.com>
2020-08-30 14:38:54 +0800

    damn gay first testing

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title></title>
-    <link href="style.css" rel="stylesheet" />
+
     <!--<header class="container">
       <li><a href="#" class="trans">Home</a></li>
       <li><a href="#" class="trans">About</a></li>
@@ -11,18 +11,63 @@
       <li><a href="#" class="trans">Challenges</a></li>
       <li><a href="#" class="trans">Contact Us</a></li>
     </header>-->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
     integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css2?family=Palanquin+Dark:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Palanquin+Dark:wght@500&display=swap" rel="stylesheet"> -->
+
+    <!-- From Index.php -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Agency - Start Bootstrap Theme</title>
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet" />
+
   </head>
 
-  <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+  <body id="page-top">
 
-        <a class="navbar-brand" href="#">Jota Joti 2020</a>
+    <!-- Navigation/Header-->
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="assets/img/navbar-logo.svg" alt="" /></a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <i class="fas fa-bars ml-1"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav text-uppercase ml-auto">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="">Home</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="">About</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="">Program</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="">Challenges</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="">Contact Us</a></li>
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+<!-- Not using this but just keep it as reference -->
+
+    <!-- <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+
+      <div class="row">
+        <a class="  navbar-brand" href="#">Jota Joti 2020</a>
+
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
+
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item active">
@@ -42,8 +87,9 @@
             </li>
           </ul>
         </div>
+      </div>
+</nav> -->
 
-</nav>
 
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
     <!-- Core theme JS-->
---
+++ home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php include "header.php"; ?>
+
+    <!-- Masthead-->
+    <header class="masthead">
+        <div class="container">
+            <div class="masthead-subheading">JOTA JOTI 2020</div>
+            <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>
+            <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
+        </div>
+    </header>
+
+
+  </body>
+</html>
