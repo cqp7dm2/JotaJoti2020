46d2c90bafab7dbe67f0a7ee92346ee5e9099b2b
cqp7dm2 <44236908+cqp7dm2@users.noreply.github.com>
2020-08-30 15:05:14 +0800

    updates

--- home.php
+++ home.php
@@ -14,6 +14,7 @@
             <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>
             <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
         </div>
+        
     </header>
 
 
