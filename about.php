0415d29e9c03d452974d87d0df873f6778686ea5
crosswind19 <70271667+crosswind19@users.noreply.github.com>
2020-09-02 23:27:29 +0800

    corrected about

--- about.php
+++ about.php
@@ -23,12 +23,16 @@
                 <span class="section-heading-lower">JOTA-JOTI</span>
                 <span class="section-heading-upper">Jamboree-on-the-Air-Jamboree-on-the-Internet</span>
               </h2>
-              <p class="mb-0">JOTA-JOTI (Jamboree-on-the-Air-Jamboree-on-the-Internet) is the world’s largest digital Scout event taking place on the Internet and over the airwaves. Held ever year in October, the event connects millions of young people around the world for a full weekend of online activities that promote friendship and global citizenship. JOTA-JOTI enables young people and volunteers to participate in fun and engaging group activities over the Internet and amateur radio focused on developing 21st century skills through Scouting.
-
-This year JOTA-JOTI 2020 will take place from 16 to 18 October. The dynamic programme features a variety of non-formal education activities, including webinars, global campfires, talent shows, live shows, and much more. World Scouting’s global partners are actively involved in the event, offering their expertise and fun activities to engage participants. The JOTA-JOTI programme aims to support Scouts and non-Scouts of all ages to learn about communications technology, the values of global citizenship, and how together we can solve the most pressing challenges facing our planet.
-
+              <p class="mb-0">JOTA-JOTI (Jamboree-on-the-Air-Jamboree-on-the-Internet) is the world’s largest digital Scout event taking place on the Internet and over the airwaves. Held ever year in October, the event connects millions of young people around the world for a full weekend of online activities that promote friendship and global citizenship. </p>
+                <p>-</p>
+                <p>JOTA-JOTI enables young people and volunteers to participate in fun and engaging group activities over the Internet and amateur radio focused on developing 21st century skills through Scouting.</p>
+                <p>-</p>
+This year JOTA-JOTI 2020 will take place from 16 to 18 October. The dynamic programme features a variety of non-formal education activities, including webinars, global campfires, talent shows, live shows, and much more. World Scouting’s global partners are actively involved in the event, offering their expertise and fun activities to engage participants.
+              <p>-</p>
+The JOTA-JOTI programme aims to support Scouts and non-Scouts of all ages to learn about communications technology, the values of global citizenship, and how together we can solve the most pressing challenges facing our planet.
+              <p>-</p>
 Keeping young people Safe from Harm online is an important aspect of this event. Through chat moderation, “listening ears”, e-learning courses and a number of sessions about how to be safe online, we make sure that all participants have a positive and inclusive JOTA-JOTI experience.
-
+              <p>-</p>
 JOTA-JOTI is one of World Scouting’s longest-running global events dating back more than 60 years to 1958. The digital educational event has seen steady annual growth in participation over the years and with a focus on the future aims to reach millions of more participants in the years to come.</p>
             </div>
           </div>
@@ -39,15 +43,15 @@ JOTA-JOTI is one of World Scouting’s longest-running global events dating back
     <section class="page-section bg-light" id="team">
         <div class="container">
             <div class="text-center">
-                <h2 class="section-heading text-uppercase">Our Amazing Team</h2>
-                <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                <h2 class="section-heading text-uppercase">Our Committee</h2>
+                <!-- <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3> -->
             </div>
             <div class="row">
                 <div class="col-lg-4">
                     <div class="team-member">
-                        <img class="mx-auto rounded-circle" src="assets/img/team/1.jpg" alt="" />
-                        <h4>Kay Garland</h4>
-                        <p class="text-muted">Lead Designer</p>
+                        <!-- <img class="mx-auto rounded-circle" src="assets/img/team/1.jpg" alt="" /> -->
+                        <h4>AJK Name 1</h4>
+                        <p class="text-muted">Position 1</p>
                         <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
                         <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
                         <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
@@ -55,9 +59,9 @@ JOTA-JOTI is one of World Scouting’s longest-running global events dating back
                 </div>
                 <div class="col-lg-4">
                     <div class="team-member">
-                        <img class="mx-auto rounded-circle" src="assets/img/team/2.jpg" alt="" />
-                        <h4>Larry Parker</h4>
-                        <p class="text-muted">Lead Marketer</p>
+                        <!-- <img class="mx-auto rounded-circle" src="assets/img/team/2.jpg" alt="" /> -->
+                        <h4>AJK name 2</h4>
+                        <p class="text-muted">Position 2</p>
                         <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
                         <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
                         <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
@@ -65,9 +69,9 @@ JOTA-JOTI is one of World Scouting’s longest-running global events dating back
                 </div>
                 <div class="col-lg-4">
                     <div class="team-member">
-                        <img class="mx-auto rounded-circle" src="assets/img/team/3.jpg" alt="" />
-                        <h4>Diana Petersen</h4>
-                        <p class="text-muted">Lead Developer</p>
+                        <!-- <img class="mx-auto rounded-circle" src="assets/img/team/3.jpg" alt="" /> -->
+                        <h4>AJK name 3</h4>
+                        <p class="text-muted">Position 3</p>
                         <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
                         <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
                         <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
@@ -75,7 +79,7 @@ JOTA-JOTI is one of World Scouting’s longest-running global events dating back
                 </div>
             </div>
             <div class="row">
-                <div class="col-lg-8 mx-auto text-center"><p class="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p></div>
+                <!-- <div class="col-lg-8 mx-auto text-center"><p class="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p></div> -->
             </div>
         </div>
     </section>
@@ -83,8 +87,8 @@ JOTA-JOTI is one of World Scouting’s longest-running global events dating back
     <section class="page-section bg-light" id="team">
         <div class="container">
             <div class="text-center">
-                <h2 class="section-heading text-uppercase">Our Amazing Team</h2>
-                <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                <!-- <h2 class="section-heading text-uppercase">Our Amazing Team</h2>
+                <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3> -->
             </div>
             <div class="row">
                 <div class="col-lg-4">
@@ -92,9 +96,9 @@ JOTA-JOTI is one of World Scouting’s longest-running global events dating back
                 </div>
                 <div class="col-lg-4">
                     <div class="team-member">
-                        <img class="mx-auto rounded-circle" src="assets/img/team/2.jpg" alt="" />
-                        <h4>Larry Parker</h4>
-                        <p class="text-muted">Lead Marketer</p>
+                        <!-- <img class="mx-auto rounded-circle" src="assets/img/team/2.jpg" alt="" /> -->
+                        <h4>AJK name 4</h4>
+                        <p class="text-muted">Position 4</p>
                         <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
                         <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
                         <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
@@ -105,7 +109,7 @@ JOTA-JOTI is one of World Scouting’s longest-running global events dating back
                 </div>
             </div>
             <div class="row">
-                <div class="col-lg-8 mx-auto text-center"><p class="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p></div>
+                <!-- <div class="col-lg-8 mx-auto text-center"><p class="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p></div> -->
             </div>
         </div>
     </section>
