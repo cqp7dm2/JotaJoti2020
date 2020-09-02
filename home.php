be68b58c5d52874b09d75a6d216db5aafb6fa897
crosswind19 <70271667+crosswind19@users.noreply.github.com>
2020-09-03 01:37:47 +0800

    home minor update

--- home.php
+++ home.php
@@ -14,9 +14,70 @@
             <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>
             <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
         </div>
+  </header>
+  <section>
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-6 order-lg-2">
+          <div class="p-5">
+            <img class="img-fluid " src="assets/img/01.jpg" alt="">
+          </div>
+        </div>
+        <div class="col-lg-6 order-lg-1">
+          <div class="p-5">
+            <h2 class="display-4">Who?</h2>
+            <p>Scouts (and Guides) of any age can participate in JOTA-JOTI. (The amateur radio stations normally need to be operated by licensed amateur radio operators.)</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-6">
+          <div class="p-5">
+            <img class="img-fluid " src="assets/img/02.jpg" alt="">
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="p-5">
+            <h2 class="display-4">When?</h2>
+            <p>JOTA-JOTI is an annual event that takes place the third weekend of October.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+        <section>
+          <div class="container">
+            <div class="row align-items-center">
+              <div class="col-lg-6 order-lg-2">
+                <div class="p-5">
+                  <img class="img-fluid " src="assets/img/header-bg.jpg" alt="">
+                </div>
+              </div>
+              <div class="col-lg-6 order-lg-1">
+                <div class="p-5">
+                  <h2 class="display-4">Scouting Around The World</h2>
+                  <p>The Scout Movement was started in England by Lord Baden Powell in 1907. Scouting is
+an educational youth movement engaging young people from all over the world to
+promote unity, respect, friendship, under- standing and peace.
+World Scouting is brought together by the World Organization of the Scout Movement
+(WOSM), which is an independent, world-wide, non-profit, non-partisan organization.
+Scouting is the largest youth movement in the world and is a worldwide family of 161
+National Scout Organizations (NSOs) in a network of over 50 million members in more
+than 1 million local community Scout Groups.
+</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
 
 
-    </header>
 
   </body>
 </html>
