19ed301e8a693925333f63ee5935f16f04760add
Nikkhesh <k.nikkheshnaiidu61@gmail.com>
2020-09-06 17:16:47 +0800

    Update program.php

--- program.php
+++ program.php
@@ -18,16 +18,16 @@
       <section class="content-section" id="portfolio">
         <div class="container">
           <div class="content-section-heading text-center">
-            <h3 class="text-secondary mb-0">Portfolio</h3>
-            <h2 class="mb-5">Recent Projects</h2>
+            <h3 class="text-secondary mb-0">Program</h3>
+            <h2 class="mb-5">Structure List</h2>
           </div>
           <div class="row no-gutters">
             <div class="col-lg-6">
               <a class="portfolio-item" href="#!">
                 <div class="caption">
                   <div class="caption-content">
-                    <div class="h2">Stationary</div>
-                    <p class="mb-0">A yellow pencil with envelopes on a clean, blue backdrop!</p>
+                    <div class="h2">Short Film Competition,THEME: YEAR 2020 </div>
+                    <p class="mb-0">smtg</p>
                   </div>
                 </div>
                 <img class="img-fluid" src="img/portfolio-1.jpg" alt="">
@@ -37,7 +37,7 @@
               <a class="portfolio-item" href="#!">
                 <div class="caption">
                   <div class="caption-content">
-                    <div class="h2">Ice Cream</div>
+                    <div class="h2">Dance Competition</div>
                     <p class="mb-0">A dark blue background with a colored pencil, a clip, and a tiny ice cream cone!</p>
                   </div>
                 </div>
@@ -48,7 +48,7 @@
               <a class="portfolio-item" href="#!">
                 <div class="caption">
                   <div class="caption-content">
-                    <div class="h2">Strawberries</div>
+                    <div class="h2">Singing Competition</div>
                     <p class="mb-0">Strawberries are such a tasty snack, especially with a little sugar on top!</p>
                   </div>
                 </div>
@@ -59,7 +59,7 @@
               <a class="portfolio-item" href="#!">
                 <div class="caption">
                   <div class="caption-content">
-                    <div class="h2">Workspace</div>
+                    <div class="h2">Group Photo Competition,THEME: WHO AM I ?</div>
                     <p class="mb-0">A yellow workspace with some scissors, pencils, and other objects.</p>
                   </div>
                 </div>
