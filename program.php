c34c9c8e768f27224723084e2f99fe0df2df71c5
Nikkhesh <k.nikkheshnaiidu61@gmail.com>
2020-09-06 11:18:56 +0800

    Update program.php

--- program.php
+++ program.php
@@ -10,10 +10,14 @@
         <header class="masthead">
 
         <div class="container">
-                <div class="masthead-subheading">JOTA JOTI 2020</div>
+                <div class="masthead-subheading">JOTA JOTI</div>
                 <div class="masthead-heading text-uppercase">Check out our programs!!</div>
             </div>
       </header>
+
+
+
+
 
   </body>
 </html>
