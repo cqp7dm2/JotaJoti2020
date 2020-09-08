fb7ab63df54fb3d2ee40bc9314c6a948942b8d28
crosswind19 <70271667+crosswind19@users.noreply.github.com>
2020-09-08 16:26:07 +0800

    committee list updated

--- about.php
+++ about.php
@@ -27,30 +27,129 @@
                 <br>
                 <p>JOTA-JOTI enables young people and volunteers to participate in fun and engaging group activities over the Internet and amateur radio focused on developing 21st century skills through Scouting.</p>
                 <br>
-<p>This year JOTA-JOTI 2020 will take place from 16 to 18 October. The dynamic programme features a variety of non-formal education activities, including webinars, global campfires, talent shows, live shows, and much more. World Scouting’s global partners are actively involved in the event, offering their expertise and fun activities to engage participants.</P>
+                <p>This year JOTA-JOTI 2020 will take place from 16 to 18 October. The dynamic programme features a variety of non-formal education activities, including webinars, global campfires, talent shows, live shows, and much more. World Scouting’s global partners are actively involved in the event, offering their expertise and fun activities to engage participants.</P>
               <br>
-<p>The JOTA-JOTI programme aims to support Scouts and non-Scouts of all ages to learn about communications technology, the values of global citizenship, and how together we can solve the most pressing challenges facing our planet.</p>
+              <p>The JOTA-JOTI programme aims to support Scouts and non-Scouts of all ages to learn about communications technology, the values of global citizenship, and how together we can solve the most pressing challenges facing our planet.</p>
               <br>
-<p>Keeping young people Safe from Harm online is an important aspect of this event. Through chat moderation, “listening ears”, e-learning courses and a number of sessions about how to be safe online, we make sure that all participants have a positive and inclusive JOTA-JOTI experience.</p>
+              <p>Keeping young people Safe from Harm online is an important aspect of this event. Through chat moderation, “listening ears”, e-learning courses and a number of sessions about how to be safe online, we make sure that all participants have a positive and inclusive JOTA-JOTI experience.</p>
               <br>
-<p>JOTA-JOTI is one of World Scouting’s longest-running global events dating back more than 60 years to 1958. The digital educational event has seen steady annual growth in participation over the years and with a focus on the future aims to reach millions of more participants in the years to come.</p>
+              <p>JOTA-JOTI is one of World Scouting’s longest-running global events dating back more than 60 years to 1958. The digital educational event has seen steady annual growth in participation over the years and with a focus on the future aims to reach millions of more participants in the years to come.</p>
             </div>
           </div>
         </div>
       </div>
     </section>
 
+
     <section class="page-section bg-light" id="team">
+      <div class="container">
+        <h2 class="section-heading text-uppercase" style="font-size: 60px">Our Committee</h2>
+        <!--Table-->
+<table id="tablePreview" class="table">
+<!--Table head-->
+  <thead style="font-size:25px">
+    <tr>
+      <th>Position</th>
+      <th>Name</th>
+    </tr>
+  </thead>
+  <!--Table head-->
+  <!--Table body-->
+  <tbody>
+    <tr>
+      <th scope="row">Advisor</th>
+      <td>Tuan Haji Zulkafli Bin Kamarudin (State Chief Commissioner) <br> <br> Mr P'ng Lai Heng (District Council Chairman)</td>
+    </tr>
+    <tr>
+      <th scope="row">District Commissioner</th>
+      <td>Mr Oi Siou Hean</td>
+    </tr>
+    <tr>
+      <th scope="row">Chairman</th>
+      <td>Mark Tan Weng Son</td>
+    </tr>
+    <tr>
+      <th scope="row">Vice Chairman</th>
+      <td>Millie Tan Chia Hui (Senior) <br> Kung Sin Jin (Junior) </td>
+    </tr>
+    <tr>
+      <th scope="row">Secretary</th>
+      <td>Thien Ruan Yuan (Senior) <br> Chin Lai Khoon (Junior) </td>
+    </tr>
+    <tr>
+      <th scope="row">Treasurer</th>
+      <td>Ooi Yeong Sheng</td>
+    </tr>
+    <tr>
+      <th scope="row">Vice Treasurer</th>
+      <td>Tan Kai Wern <br> <br> Koay Zi Jie</td>
+    </tr>
+    <tr>
+      <th scope="row">Program Unit (Senior)</th>
+      <td>Ch'ng Zheng Yuen (Leader) <br> Chong Hui Shien <br> Eva Teng Ee Hwa <br> Poh Zi Jun <br> Rachel Tan Chia Yi </td>
+    </tr>
+    <tr>
+      <th scope="row">Program Unit (Junior)</th>
+      <td>Ng Kah Weng <br> Tee Sok Huan <br> Yeoh Kean Keong </td>
+    </tr>
+    <tr>
+      <th scope="row">JOTA (Radio Station)</th>
+      <td>Charlie Teoh theam Chye <br> Leonard Khor Kean Jynn </td>
+    </tr>
+    <tr>
+      <th scope="row">Challenge Master</th>
+      <td>Chan Yaou Ming <br> Lim Wan Hao</td>
+    </tr>
+    <tr>
+      <th scope="row">Media and Publicity</th>
+      <td>Ang Chern Xing <br> Eric Goh Thean Yu <br> Kyara Khaw Wen Qian <br> Rachel Tan Chia Yi </td>
+    </tr>
+    <tr>
+      <th scope="row">Sponsorship</th>
+      <td>Chong Wen Jun <br> <br>Hoe Dong Liang</td>
+    </tr>
+    <tr>
+      <th scope="row">Merchandising Donation and Distribution</th>
+      <td>Axzel Tan Aik Sian <br> Lee Pei Nee </td>
+    </tr>
+    <tr>
+      <th scope="row">ICT</th>
+      <td>Chua Qin Peng (Leader) <br> Cheah Kah Xuan <br> Eva Teng Ee Hwa <br> Lim Chee Hoong <br> Nikkhesh Naiidu </td>
+    </tr>
+    <tr>
+      <th scope="row">Prize and Souvenir</th>
+      <td>Boon Yoong Theng</td>
+    </tr>
+    <tr>
+      <th scope="row">Designer</th>
+      <td>Fong Jia Wei</td>
+    </tr>
+    <tr>
+      <th scope="row">Auditor</th>
+      <td>Loo Chin Choong</td>
+    </tr>
+  </tbody>
+  <!--Table body-->
+</table>
+<!--Table-->
+      </div>
+    </section>
+
+    <!-- .....................................................
+    If future need to put personal photo can use this template
+    ........................................................... -->
+
+    <!-- <section class="page-section bg-light" id="team">
         <div class="container">
             <div class="text-center">
                 <h2 class="section-heading text-uppercase">Our Committee</h2>
                 <!-- <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3> -->
-            </div>
+            <!-- </div>
             <div class="row">
                 <div class="col-lg-4">
-                    <div class="team-member">
+                    <div class="team-member"> -->
                         <!-- <img class="mx-auto rounded-circle" src="assets/img/team/1.jpg" alt="" /> -->
-                        <h4>AJK Name 1</h4>
+                        <!-- <h4>AJK Name 1</h4>
                         <p class="text-muted">Position 1</p>
                         <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
                         <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
@@ -58,9 +157,9 @@
                     </div>
                 </div>
                 <div class="col-lg-4">
-                    <div class="team-member">
+                    <div class="team-member"> -->
                         <!-- <img class="mx-auto rounded-circle" src="assets/img/team/2.jpg" alt="" /> -->
-                        <h4>AJK name 2</h4>
+                        <!-- <h4>AJK name 2</h4>
                         <p class="text-muted">Position 2</p>
                         <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
                         <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
@@ -68,9 +167,9 @@
                     </div>
                 </div>
                 <div class="col-lg-4">
-                    <div class="team-member">
+                    <div class="team-member"> -->
                         <!-- <img class="mx-auto rounded-circle" src="assets/img/team/3.jpg" alt="" /> -->
-                        <h4>AJK name 3</h4>
+                        <!-- <h4>AJK name 3</h4>
                         <p class="text-muted">Position 3</p>
                         <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
                         <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
@@ -78,41 +177,11 @@
                     </div>
                 </div>
             </div>
-            <div class="row">
+            <div class="row"> -->
                 <!-- <div class="col-lg-8 mx-auto text-center"><p class="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p></div> -->
             </div>
         </div>
-    </section>
-
-    <section class="page-section bg-light" id="team">
-        <div class="container">
-            <div class="text-center">
-                <!-- <h2 class="section-heading text-uppercase">Our Amazing Team</h2>
-                <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3> -->
-            </div>
-            <div class="row">
-                <div class="col-lg-4">
-
-                </div>
-                <div class="col-lg-4">
-                    <div class="team-member">
-                        <!-- <img class="mx-auto rounded-circle" src="assets/img/team/2.jpg" alt="" /> -->
-                        <h4>AJK name 4</h4>
-                        <p class="text-muted">Position 4</p>
-                        <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
-                        <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
-                        <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
-                    </div>
-                </div>
-                <div class="col-lg-4">
-
-                </div>
-            </div>
-            <div class="row">
-                <!-- <div class="col-lg-8 mx-auto text-center"><p class="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p></div> -->
-            </div>
-        </div>
-    </section>
+    </section> -->
 
 
   </body>
