6adce9aa32ae3a3b608cb9fce9e5b3ce65abd256
Nikkhesh <k.nikkheshnaiidu61@gmail.com>
2020-09-09 14:05:52 +0800

    Update program.php

--- program.php
+++ program.php
@@ -110,10 +110,40 @@
           </P>
 
 
-        <h3>Dance Competition</h3>
-         <p> 1)	NUMBER OF PARTICIPANTS (size of the crews): <br><br>
-            ●	5-10 members.<br>
-
+          <h3>Short Film Competition,THEME: Year 2020</h3>
+          <p> This year has not been an easy one for all of us. Small businesses were affected,
+            thousands of lives were lost, school was cancelled, and many of our long-term plans for the year were put on hold.
+            How did 2020 impact you? Create a short film on what 2020 has taught you, your hopes and dreams for the rest of
+            the year, or a reenactment of this year itself. As long as it is according to the theme,
+            let your ideas roam freely! <br><br>
+            1)	DURATION:<br>
+            	●	The total runtime of the short film must be twelve (12) minutes. This includes the beginning and ending credits. <br><br>
+            2)	LANGUAGE:  <br>
+              ●	Bahasa Malaysia, English, Mandarin languages are accepted.  <br>
+              ●	Subtitles in English are compulsory. <br><br>
+            3) TOPIC SENSITIVITY: <br>
+              ●	All films cannot contain any sensitive subjects such as religion, race and politics. Vulgarities and inappropriate clothing are not allowed. <br><br>
+            4)	COPYRIGHT: <br>
+            ●	The short film submitted must not contain any copyrighted work(s) belonging to any third parties unless,<br>
+            (a) Participants have a license to use such works in the Film; or <br>
+            (b) the use participants’ Film makes of such works is a fair one under the copyright laws.<br>
+            ●	However if the video is taken down by Facebook due to copyright issues, the organizing committee will not bear responsibility for entries that are not received or posted for whatever reason <br><br>
+            5) 	DEADLINE: <br>
+            ●	The deadline for submission of film is 1159 hours (+8 GMT) 9 October 2020.<br>
+            ●	 Films received after this deadline will not be viewed or rated by the judging panel.<br><br>
+            6) 	SUBMISSION:<br>
+            ●	All video submissions must be in MP4 format, minimum 720p, fps 24hz.<br>
+            ●	Judges decision will be final and binding<br>
+            ●	Submissions should be done via Google Drive with file name "TROOPNAME_SHORTFILM"<br>
+            ●	Provide a list of participants’ details which contains their names, job title, age and email address in ONE Microsoft Word document per video submission and submit via Google Drive with file name "TROOPNAME_SHORTFILM_PARTICIPANT_DETAILS.doc".<br><br>
+            ●	Kindly send the Google Drive link to the email Joinjota@scout.my and CC rachcytan@gmail.com .
+            ●	Kindly contact the in-charge person of particular competition for questioning. Facebook URL: https://www.facebook.com/rachel.tan.7796/ OR Email: rachcytan@gmail.com.<br><br>
+            7) PRIZES: <br>
+            ●	Best Actor<br>
+            ●	Best Actress<br>
+            ●	Best Film<br>
+            ●	People’s Choice Award<br><br>
+          </P>
 
           </p>
 
