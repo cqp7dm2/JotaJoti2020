c84d68a9365da227b06516c65aa909a0cd04da77
crosswind19 <70271667+crosswind19@users.noreply.github.com>
2020-09-11 23:58:50 +0800

    .

--- program.php
+++ program.php
@@ -71,6 +71,132 @@
         </div>
       </section>
 
+      <section class="page-section" id="services">
+        <div class="container">
+
+        </div>
+
+      </section>
+
+      <section id="features" class="features">
+        <div class="container">
+          <h3>Short Film Competition, THEME: Year 2020</h3>
+          <p class="futura"> This year has not been an easy one for all of us. Small businesses were affected,
+            thousands of lives were lost, school was cancelled, and many of our long-term plans for the year were put on hold.
+            How did 2020 impact you? Create a short film on what 2020 has taught you, your hopes and dreams for the rest of
+            the year, or a reenactment of this year itself. As long as it is according to the theme,
+            let your ideas roam freely! <br><br>
+
+
+          <div class="row">
+            <div class="col-lg-3">
+              <ul class="nav nav-tabs flex-column">
+                <li class="nav-item">
+                  <a class="nav-link active show" data-toggle="tab" href="#tab-1">DURATION</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" data-toggle="tab" href="#tab-2">LANGUAGE</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" data-toggle="tab" href="#tab-3">TOPIC SENSITIVITY</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" data-toggle="tab" href="#tab-4">COPYRIGHT</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" data-toggle="tab" href="#tab-5">DEADLINE</a>
+                </li>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" data-toggle="tab" href="#tab-6">SUBMISSION</a>
+              </li>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" data-toggle="tab" href="#tab-7">PRIZES</a>
+            </li>
+              </ul>
+            </div>
+            <div class="col-lg-9 mt-4 mt-lg-0">
+              <div class="tab-content">
+                <div class="tab-pane active show" id="tab-1">
+                  <div class="row">
+                    <div class="col-lg-8 details order-2 order-lg-1">
+                      <h3>DURATION</h3>
+                      <p>The total runtime of the short film must be twelve (12) minutes. This includes the beginning and ending credits.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="tab-pane" id="tab-2">
+                  <div class="row">
+                    <div class="col-lg-8 details order-2 order-lg-1">
+                      <h3>LANGUAGE</h3>
+                      <p>   ●	Bahasa Malaysia, English, Mandarin languages are accepted.  <br>
+                            ●	Subtitles in English are compulsory. <br><br></p>
+                    </div>
+                  </div>
+                </div>
+                <div class="tab-pane" id="tab-3">
+                  <div class="row">
+                    <div class="col-lg-8 details order-2 order-lg-1">
+                      <h3>TOPIC SENSITIVITY</h3>
+                      <p>All films cannot contain any sensitive subjects such as religion, race and politics. Vulgarities and inappropriate clothing are not allowed.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="tab-pane" id="tab-4">
+                  <div class="row">
+                    <div class="col-lg-8 details order-2 order-lg-1">
+                      <h3>COPYRIGHT</h3>
+                      <p> ●	The short film submitted must not contain any copyrighted work(s) belonging to any third parties unless,<br>
+                          (a) Participants have a license to use such works in the Film; or <br>
+                          (b) the use participants’ Film makes of such works is a fair one under the copyright laws.<br>
+                          ●	However if the video is taken down by Facebook due to copyright issues, the organizing committee will not bear responsibility for entries that are not received or posted for whatever reason <br><br>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="tab-pane" id="tab-5">
+                  <div class="row">
+                    <div class="col-lg-8 details order-2 order-lg-1">
+                      <h3>DEADLINE</h3>
+                      <p> ●	The deadline for submission of film is 1159 hours (+8 GMT) 9 October 2020.<br>
+                          ●	 Films received after this deadline will not be viewed or rated by the judging panel.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="tab-pane" id="tab-6">
+                  <div class="row">
+                    <div class="col-lg-8 details order-2 order-lg-1">
+                      <h3>SUBMISSION</h3>
+                      <p> ●	All video submissions must be in MP4 format, minimum 720p, fps 24hz.<br>
+                          ●	Judges decision will be final and binding<br>
+                          ●	Submissions should be done via Google Drive with file name "TROOPNAME_SHORTFILM"<br>
+                          ●	Provide a list of participants’ details which contains their names, job title, age and email address in ONE Microsoft Word document per video submission and submit via Google Drive with file name "TROOPNAME_SHORTFILM_PARTICIPANT_DETAILS.doc".<br>
+                          ●	Kindly send the Google Drive link to the email Joinjota@scout.my and CC rachcytan@gmail.com .<br>
+                          ●	Kindly contact the in-charge person of particular competition for questioning. Facebook URL: https://www.facebook.com/rachel.tan.7796/ OR Email: rachcytan@gmail.com.<br><br>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="tab-pane" id="tab-7">
+                  <div class="row">
+                    <div class="col-lg-8 details order-2 order-lg-1">
+                      <h3>PRIZES</h3>
+                      <p> ●	Best Actor<br>
+                          ●	Best Actress<br>
+                          ●	Best Film<br>
+                          ●	People’s Choice Award
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </section><!-- End Features Section -->
+
 
       <section class="page-section" id="services">
         <div class="container">
