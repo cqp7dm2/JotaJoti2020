e5fa2d39b9d3d245ef6c23e8633527e29723c196
cqp7dm2 <44236908+cqp7dm2@users.noreply.github.com>
2020-09-14 18:52:02 +0800

    minor editing on footer

--- footer.php
+++ footer.php
@@ -25,74 +25,6 @@
   </head>
   <body>
 
-    <!--<footer class="footer py-4">
-      <div class="container" style="height: 50px;">
-        <div class="row align-items-center mb-5">
-          <div class="col-lg-3 text-lg-left">Logo</div>
-          <div class="col-lg-3 text-lg-left">
-            <ul>
-
-              <li><a href="#">Home</a></li>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Program</a></li>
-              <li><a href="#">Challenges</a></li>
-              <li><a href="#">Contact Us</a></li>
-            </ul>
-          </div>
-          <div class="col-lg-3 text-lg-left">
-
-          </div>
-          <div class="col-lg-3 my-3 my-lg-0">
-            <a class="btn btn-light btn-social mx-2" href="#!"><i class="fab fa-instagram"></i></a>
-            <a class="btn btn-light btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
-          </div>
-        </div>
-        <div class="row align-items-center">
-          <div class="col-lg-12 text-lg-center">Copyright © Jota Joti 2020</div>
-        </div>
-      </div>
-    </footer>
-
-    <div class="">
-    <section style="height:80px;"></section>
-    <footer class="footer-bs">
-        <div class="row">
-        	<div class="col-md-3 footer-brand animated fadeInLeft">
-            	<h2>Jota Joti 2020</h2>
-                <p>Suspendisse hendrerit tellus laoreet luctus pharetra. Aliquam porttitor vitae orci nec ultricies. Curabitur vehicula, libero eget faucibus faucibus, purus erat eleifend enim, porta pellentesque ex mi ut sem.</p>
-                <p>Copyright © Jota Joti 2020, All rights reserved</p>
-            </div>
-        	<div class="col-md-3 footer-nav animated fadeInUp">
-            	<h4>Menu —</h4>
-            	<div class="col-md-6">
-                    <ul class="list">
-                        <li><a href="home.php"><h4>Home</h4></a></li>
-                        <li><a href="about.php"><h4>About</h4></a></li>
-                        <li><a href="program.php"><h4>Program</h4></a></li>
-                        <li><a href="challenges.php"><h4>Challenges</h4></a></li>
-                        <li><a href="contactus.php"><h4>Contact Us</h4></a></li>
-                    </ul>
-                </div>
-            </div>
-        	<div class="col-md-2 footer-social animated fadeInDown">
-            	<h4>Follow Us</h4>
-            	<ul>
-                	<li><a href="#"><h4>Facebook</h4></a></li>
-                	<li><a href="#"><h4>Instagram</h4></a></li>
-                </ul>
-            </div>
-        	<div class="col-md-3 footer-ns animated fadeInRight">
-            <li><a href="#"><h4 style="color: #fff;">Terms and Condition</h4></a></li>
-            <li><a href="#"><h4 style="color: #fff;">FAQ</h4></a></li>
-            <li><a href="#"><h4 style="color: #fff;">Cookies Policy</h4></a></li>
-            <li><a href="#"><h4 style="color: #fff;">Privacy Policy</h4></a></li>
-            <li><a href="#"><h4 style="color: #fff;">Terms of Use</h4></a></li>
-          </div>
-        </div>
-    </footer>
-
-</div>-->
-
 <footer class="footer py-4">
           <div class="container">
               <div class="row align-items-center" style="color: #fff;">
@@ -144,9 +76,13 @@
               </div>
               <div class="row text-center py-5" style="color: #fff; font-size: 13px;">
                 <div class="col-12">
-                  Copyright © Jota Joti 2020, All rights reserved
+                  Copyright © Jota Joti 2020, All rights reserved | Powered by <a href="https://www.scout.my/" style="color:white" target="_blank">GTS Penang</a>
+                  <hr>
+                  Design and Develop by <a href="#" style="color:white" target="_blank">IT Team @ Jota Joti 2020</a>
                 </div>
               </div>
+
+
           </div>
       </footer>
 
