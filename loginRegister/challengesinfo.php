05098313908b79af32cc6d5e399bcd46b333af08
cqp7dm2 <44236908+cqp7dm2@users.noreply.github.com>
2020-09-14 23:20:06 +0800

    final

--- loginRegister/challengesinfo.php
+++ loginRegister/challengesinfo.php
@@ -26,17 +26,14 @@
   gtag('config', 'UA-177875132-1');
 </script>
 
-    <?php
-    SESSION_start()
-     ?>
-
-     <?php
-     // FOR USERS TO VALIDATION ON THE LOGIN
-     //AVOID USER COME INTO PAGE WITHOUT LOGIN
-       if(empty($_SESSION['user_id'])) {
-       header("Location:login.php");
-       }
-      ?>
+<?php
+SESSION_start();
+// FOR USERS TO VALIDATION ON THE LOGIN
+//AVOID USER COME INTO PAGE WITHOUT LOGIN
+  if(empty($_SESSION['u_id'])) {
+  header("Location:login.php");
+  }
+ ?>
 
     <title>Jota-Joti 2020|Challenges Information</title>
     <link rel="icon" href="../assets/img/2020.png">
